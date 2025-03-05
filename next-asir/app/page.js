baaeb6aca7c170e80d63b1f961452aaac9859ddf
FabioFabrega <fabiofabrega746@gmail.com>
2025-03-05 09:52:59 +0100

    avances

--- next-asir/app/page.js
+++ next-asir/app/page.js
@@ -1,9 +1,11 @@
 'use client'
+import { FaBasketShopping } from "react-icons/fa6";
 import Link from "next/link";
 import "./globals.css";
 import { IoPersonCircle } from "react-icons/io5";
+import Carrusel from "./carrusel/page";
 
-async function getLibros() {
+  async function getLibros() {
   const res = await fetch("http://localhost:4000/libro");
   if (!res.ok) throw new Error("Failed to fetch libros");
   return res.json();
@@ -16,21 +18,19 @@ export default async function RootLayout() {
     <html>
       <body >
         <header>
-        <nav>
-        <h1>Tienda en desarrollo</h1>
-        <div className="div2">
-          <input type="text" id="texto" name="texto" placeholder="Busca aqui..." />
-        </div>
-        <div><Link href="/"><h2>Mi cuenta</h2></Link></div>
-          </nav></header>
+          <nav>
+            <h1>Tienda en desarrollo</h1>
+            <input type="text" id="texto" name="texto" placeholder="Busca aqui..." />
+            <h3><Link href="/"><IoPersonCircle/>Mi cuenta</Link><p></p><Link href="/"><FaBasketShopping/>Mi cesta</Link></h3>
+          </nav>
+        </header>
+        <div className="divC"><Carrusel/></div>
         <div>
         {libros.map((libro) => (
           <Link href={`/bibliotecatic/libros/${libro.id}`} key={libro.id}>
-              <div>
-                <img src={libro.portadaUrl || "/file.svg"} alt={libro.titulo} />
-                <h2>{libro.titulo}</h2>
+                <img className="product-img"src={libro.portadaUrl || "/file.svg"} alt={libro.titulo} />
+                <h4>{libro.titulo}</h4>
                 <p>{libro.autor?.nombre && `Autor: ${libro.autor.nombre}`}</p>
-              </div>
           </Link>
         ))}
       </div>
