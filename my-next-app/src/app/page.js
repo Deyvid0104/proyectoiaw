f8e9f4400ccb9e5021099a5b46bce973bd6dbf21
FabioFabrega <fabiofabrega746@gmail.com>
2025-03-06 10:58:22 +0100

    avance pagina

--- my-next-app/src/app/page.js
+++ my-next-app/src/app/page.js
@@ -3,21 +3,44 @@ import { FaBasketShopping } from "react-icons/fa6";
 import Link from "next/link";
 import "./globals.css";
 import { IoPersonCircle } from "react-icons/io5";
+import { Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function Home() {
   return (
-    <html lang="en">
-        <header>
-          <nav>
-            <h1>Tienda en desarrollo</h1>
-            <input type="text" id="texto" name="texto" placeholder="Busca aqui..." />
-            <h3><Link href="/"><IoPersonCircle/>Mi cuenta</Link><p>
-              </p><Link href="/"><FaBasketShopping/>Mi cesta</Link></h3>
-          </nav></header>
-         <main><aside>
-            <h2>prueba</h2>
-          </aside></main>
-        <body>
-      </body>
-    </html>
+    <div>
+      <header>
+        <nav>
+          <h1>Tienda en desarrollo</h1>
+          <input type="text" id="texto" name="texto" placeholder="Busca aqui..." />
+          <h3><Link href="/"><IoPersonCircle />Mi cuenta</Link><p>
+          </p><Link href="/"><FaBasketShopping />Mi cesta</Link></h3>
+        </nav></header>
+      <main><aside>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Categoría
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <button >
+                Ordenadores
+              </button>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <button >
+                Moviles
+              </button>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <button >
+                Teléfonos
+              </button>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <h2><Link href="/">About</Link></h2>
+      </aside><section><h1>Prueba</h1></section></main>
+    </div>
   );
 }
